41db77ba7a3cb10d57543b54c3e55b0cd08b58ca
Holy-Newton <deberdtfrancois@icloud.com>
2025-04-06 04:12:20 +0200

    MENU

--- gravitational_trajectory.py
+++ gravitational_trajectory.py
@@ -8,9 +8,11 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gravitational Trajectory Simulation by Holly Newton")
 
 
-#constants
+# CONSTANT
 epsilon = 2e6  ### epsilon is the "softening" to avoid singularities. (logical)#
 G = 6.67430e-9
+teta = 0.1
+fact = 1
 # Commonly used colors in programming
 BLACK = (0, 0, 0)       # Noir
 WHITE = (255, 255, 255) # Blanc
@@ -19,6 +21,7 @@ GREEN = (0, 255, 0)     # Vert
 BLUE = (150, 150, 255)      # Bleu
 YELLOW = (255, 255, 0)  # Jaune
 CYAN = (0, 255, 255)    # Cyan
+TRON = (167, 208, 255)
 
 class Star:
     def __init__(self, name, mass, radius, color, position, velocity, trace_val):
@@ -54,10 +57,112 @@ class Star:
         print(self.position)
 
 
+### _______________MENU()________________________
+
+def draw_text(win, text, font_size, position, color):
+    font = pygame.font.SysFont('courier new', font_size)
+    font.set_bold(True)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect(center=position)
+    win.blit(text_surface, text_rect)
+
+def chargement(x , y, teta, fact):
+    
+    n = int(teta/(2*m.pi) * 255)
+    a,b = x,y
+    radius = 14
+    if fact == 1:
+        x += radius * np.cos(teta)
+        y += radius * np.sin(teta)
+        x1 = a+(-radius) * np.cos(teta)
+        y1 = b+(-radius) * np.sin(teta)
+    else:
+        x += radius * np.cos(2*m.pi-teta)
+        y += radius * np.sin(2*m.pi-teta)
+        x1 = a+(-radius) * np.cos(-teta)
+        y1 = b+(-radius) * np.sin(-teta)
+    print(n)
+    pygame.draw.circle(win, (255-n/2, 255-n, 255), (x,y), 10)
+    pygame.draw.circle(win, (n/1.3, 40, 90), (x1,y1), 10)
+    
+
+def menu():
+    global quit_menu
+    clock = pygame.time.Clock()
+    run = True
+    WB, HB = 500,100
+    global teta
+    global fact
+    while run:
+
+        ### CHARGEMENT
+        if teta >= 2*m.pi or teta <= 0:
+            fact = -fact
+            teta += fact*0.1
+        else: teta += fact*0.1
+        clock.tick(60)
+        win.fill(BLACK)
+
+        ### TITLE
+
+        title_square = pygame.Rect(58, 57, WIDTH*0.90, 100)
+        title_square1 = pygame.Rect(58+4, 57+4, WIDTH*0.90-8, 92)
+        pygame.draw.rect(win, TRON, title_square, border_radius=10)
+        pygame.draw.rect(win, BLACK, title_square1, border_radius=10)
+        font = pygame.font.SysFont('courier new', 29)
+        font.set_underline(True)
+        font.set_bold(True)
+        text = font.render("==Gravitational Trajectory Simulation====@Holy_Newton==", 1, TRON)
+        win.blit(text, (WIDTH/2 - text.get_width()/2, 130 - text.get_height()/1))
+
+
+        mouse_pos = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        # START BUTTON
+        
+        start_but = pygame.Rect(550/2-WB/2, HEIGHT-150-HB/2+50,WB, HB)
+        start_but2 = pygame.Rect(550/2-WB/2+4, HEIGHT-150+4-HB/2+50, WB-8, HB-8)
+        pygame.draw.rect(win, TRON, start_but, border_radius=10)
+        pygame.draw.rect(win, BLACK, start_but2, border_radius=10)
+
+        if start_but.collidepoint(mouse_pos):
+            WB, HB = 515,115
+            draw_text(win, "Start Simulation", 38, (250, HEIGHT-100), TRON)
+            if click[0]:
+                draw_text(win, "Start Simulation", 32, (250, HEIGHT-100), TRON)
+                WB, HB = 485,85
+        else:
+            draw_text(win, "Start Simulation", 35, (250, HEIGHT-100), TRON)
+            WB, HB = 500,100
+        
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:  # Left mouse button
+                    if start_but.collidepoint(mouse_pos):
+                        run = False
+
+            if event.type == pygame.QUIT:
+                run = False
+                quit_menu = True
+        
+        chargement(480, HEIGHT-97 , teta, fact)
+
+        
+        pygame.display.update()
+
+
+### _______________MAIN()________________________
+
+
 def main():
+    quit_menu = False
+    menu()
     run = True
     clock = pygame.time.Clock()
     dt = 0.7
+    
     
     #----------------###  STARS   ###------------------
 
@@ -111,6 +216,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        if quit_menu:
+            run = False
+
         for star in star_system:
             star.new_position(dt, star_system)
             star.draw(win)
